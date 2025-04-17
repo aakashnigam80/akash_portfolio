b15def05685726bb82b0448ad62773b207bb43ca
aakashnigam80 <akashnigam0911@gmail.com>
2025-04-17 17:24:31 +0530

    updated fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,6 +28,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#FBFBFA]`}
       >
         <main className="p-4 sm:p-0 min-h-screen">{children}</main>
+        <Analytics />
       </body>
     </html>
   );
